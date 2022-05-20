71e32e07deb51d601ff0b3e69ba4070713143314
tyler-cranmer <tyler.cranmer@outlook.com>
2022-05-19 23:36:04 -0400

    mapping

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import pytesseract
 import os
+import re
 
 
 def ocr_core(img):
@@ -20,15 +21,33 @@ def thresholding(image):
     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 directory = 'images'
-
+src = ''
+accounts = []
+refined = []
 for filename in os.scandir(directory):
     if filename.is_file():
         img = cv2.imread(filename.path)
         img = get_grayscale(img)
         img = thresholding(img)
         img = remove_noise(img)
-        print(ocr_core(img))
-        break
 
+        src = ocr_core(img)
+        # pattern = '/(?<!\w)@\w+/'
+        # result = re.findall(pattern, src)
+        # print(result)
+    # [accounts.append(word) for word in src.split() if word.startswith('@')]
+    for word in src.split():
+        if (word.startswith('@') and (word.rfind('4') < 0 or word.rfind('&') < 0)):
+            accounts.append(word)
+    break
+
+print(accounts)
+print(len(accounts))
+
+
+# [refined.append(name) for name in accounts if len(name) < 3]
+
+# print(refined)
+# print(len(refined))
 
 
